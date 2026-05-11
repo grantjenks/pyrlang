43f4cbadcef39e5c2173f142b9865b77be852914
Grant Jenks <grant.jenks@gmail.com>
2026-05-10 21:04:40 -0700

    Publish release wheels to PyPI

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ OTP_APPS = ("kernel", "stdlib", "crypto")
 
 
 def package_version() -> str:
-    return os.environ.get("PYRLANG_VERSION", "0.1.0")
+    return os.environ.get("PYRLANG_VERSION", "0.1.1")
 
 
 def erl_eval(expr: str) -> str:
